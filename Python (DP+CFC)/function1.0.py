10b24b76b4b5cb2b7fa4f0cfe241dabd06776208
Nischal <nischalshrestha800@gmail.com>
2024-04-25 19:18:14 +0545

    Different ways of Functions Making

--- Python (DP+CFC)/function1.0.py
+++ Python (DP+CFC)/function1.0.py
@@ -1,10 +1,28 @@
-def greet():
-    print("Hello, Rebeka") 
-greet()    
+# def greet():
+#     print("Hello, Rebeka") 
+# greet()    
 
-def greet(name):
-    print(f"Namaste, {name}") 
-greet("Ronaldo")    
-greet("Nischal")    
-greet("Cristiano")    
+# def greet(name):
+#     print(f"Namaste, {name}") 
+# greet("Ronaldo")    
+# greet("Nischal")    
+# greet("Cristiano")    
 
+# WAY 1:
+def addition(m,n):
+    return m+n
+print(addition(4,5))    
+
+# WAY 2:
+def sum(m,n):
+    # return n+10
+    print(m+n+10)
+sum(10,10)    
+# sum(10)
+
+# WAY 3:
+def add(a,b):
+    sum=a+b
+    print("The sum of two entered numbers is:",sum)
+
+add(6,4)
