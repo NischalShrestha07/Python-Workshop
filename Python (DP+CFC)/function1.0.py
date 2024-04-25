f4e768ff87f85fda1396011b7f44c49dae23dafc
Nischal <nischalshrestha800@gmail.com>
2024-04-25 19:38:29 +0545

    *args Function important One

--- Python (DP+CFC)/function1.0.py
+++ Python (DP+CFC)/function1.0.py
@@ -33,8 +33,33 @@ add(6,4)
 join=lambda a,b:a+b
 print(join(45,5))
 
+
+
 #   WAY-> 5
 # IMPORTANT ARGUMENT TO TAKE MULTIPLE ARGUMENTS:(*args)
 def test(*args):
     print(args)
 test(1,2,3,4,5,6,7)    
+
+
+def masterAdd(*letsgo):
+    print(letsgo)
+masterAdd(1,2,3,4,5,6,7)    
+
+# Check Even/Odd Using functions
+def number(n):
+    if(n%2==0):
+        print("Even Number")
+    else:
+        print("Odd Number")
+number(15)        
+number(53)        
+number(19)        
+number(14)        
+number(16)        
+
+num=454
+n2=454
+print(num==n2)
+
+            
