bb5bc83c62611791174dcc5e26a2df339385da1d
Nischal <nischalshrestha800@gmail.com>
2024-04-26 21:25:53 +0545

    Encapsulation Details with Examples

---
+++ Python (DP+CFC)/encapsulation1.1.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self,name,em_id,salary):
+        # _name the underscore(_) is given before so that these data cannot be accessed outside this class/any person.
+        self._name=name
+        self._em_id=em_id
+        self._salary=salary
+    def display_info(self):
+        print(f"Name: ${self._name}") 
+        print(f"Employee ID: ${self._em_id}") 
+        print(f"Salary: ${self._salary}") 
+        
+        
+# employee._name By this the data cannot be accessed as _ is used which means its encapsulated.
+# So to get those data we need getter() function as we have made display_info() method
+employee=Employee("Naruto",1,1000)        
+employee.display_info()
