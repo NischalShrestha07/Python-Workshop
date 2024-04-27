1d4fccf6b619c3552179f5c0f5b6bd5bd341fa54
Nischal <nischalshrestha800@gmail.com>
2024-04-27 19:53:15 +0545

    Inheritance

--- Python (DP+CFC)/inheritance1.1.py
+++ Python (DP+CFC)/inheritance1.1.py
@@ -1,18 +1,19 @@
 
 
-class Vehicle:
-    def __init__(self,name,brand,color,price):
-        self.name=name
-        self.brand=brand
-        self.color=color
-        self.price=price
+# class Vehicle:
+#     def __init__(self,name,brand,color,price):
+#         self.name=name
+#         self.brand=brand
+#         self.color=color
+#         self.price=price
 
-    def move(self):
-        print("The car is moving")   
+#     def move(self):
+#         print("The car is moving")   
 
-    def stop(self):
-        print("The car has stopped")   
-        
- class Car(Vehicle):
+#     def stop(self):
+#         print("The car has stopped")   
+#     def printName(self):
+#         print(f"")    
+#  class Car(Vehicle):
+#      def
             
-        
