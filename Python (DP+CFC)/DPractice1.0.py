c792308e663e206d162155fe60823a7eed206c1b
Nischal <nischalshrestha800@gmail.com>
2024-04-28 15:39:07 +0545

    Day1 Practical Practices

--- Python (DP+CFC)/DPractice1.0.py
+++ Python (DP+CFC)/DPractice1.0.py
@@ -1,13 +1,23 @@
 
-fruits=["apple","banana","cheery","kiwi","mango"]
-newList=[]
+# fruits=["apple","banana","cheery","kiwi","mango"]
+# newList=[]
 
-for x in fruits:
-    if 'a' in x:
-        newList.append(x)
+# for x in fruits:
+#     if 'a' in x:
+#         newList.append(x)
 
-print(newList)
+# print(newList)
 
 
-newList1=[x for x in fruits if "go" in x]
-print(newList1)
+# newList1=[x for x in fruits if "go" in x]
+# print(newList1)
+
+
+list1=[]
+num=int(input("Enter numbers of elements in list:"))
+
+for i in range(num):
+    els =int(input("Enter elememts:"))
+    list1.append(els)
+
+    print("Largest elemet is:",max(list1))
