6ec1bafb8254389f5cdb08a5deb424183494350b
Nischal <nischalshrestha800@gmail.com>
2024-04-28 12:04:59 +0545

    Encapsulations Practices

--- Python (DP+CFC)/encapsulation1.2.py
+++ Python (DP+CFC)/encapsulation1.2.py
@@ -6,17 +6,24 @@ class BankAccount:
         self.balance=balance
     def deposit(self,amount):
         self.balance=self.balance+amount
-        print(f"Deposit SuccessFul. New Balance: ${self.balance}")
+        print(f"Deposit SuccessFul.\nNew Balance: ${self.balance}")
     
     
     def checkBalance(self):
         print(f"Current Balance: {self.balance}")    
-    
+    def transferAmount(self,send):
+        self.balance=self.balance-send
+        print(f"Transfered Successful.\nNew balance:{self.balance}")
     
     def withdraw(self,amount):
         self.balance=self.balance-amount
+        print(f"Withdraw SuccessFul\nNew Balance: {self.balance}")
 
 
 print("\n")        
-account=BankAccount("12540",100000)
-account.checkBalance()
+account=BankAccount("12540",10000)
+# account.checkBalance()
+# account.withdraw(5000)
+account.transferAmount(5000)
+account.deposit(30000)
+account.withdraw(50000)
