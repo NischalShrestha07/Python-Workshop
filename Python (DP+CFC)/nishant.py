79d5361b0bdc1c498b6fed782a1980ebebf21a1f
Nischal <nischalshrestha800@gmail.com>
2024-04-28 20:09:28 +0545

    Count letter in Sentences

--- Python (DP+CFC)/nishant.py
+++ Python (DP+CFC)/nishant.py
@@ -1,2 +1,6 @@
 
-print("Nishant Shrestha")
+sentence=input("enter the sentence:")
+letter=input("Enter the letter to count:")
+# To count the number of repeated letter in the sentence
+print(sentence.count(letter))
+
