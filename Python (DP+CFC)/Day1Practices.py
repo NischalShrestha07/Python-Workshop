7c910f9a120ae08e4f378581005bc33006a22b0d
Nischal <nischalshrestha800@gmail.com>
2024-04-28 14:25:48 +0545

    Day1 Practical Practices

---
+++ Python (DP+CFC)/Day1Practices.py
@@ -0,0 +1,38 @@
+
+
+# for i in "Data":
+#     print(i)
+#     break
+# a="Data Science"
+# print(len(a))
+
+# # Boolean types
+# txt ="I ate Banana"
+# print("eat" in txt)
+
+
+# if "banana".capitalize() in txt:
+#     print("Banana is Present")
+# else:
+#     print("Banana is not Present")
+       
+# #Below is boolean type which is True Statements so It Returns True
+
+# print("expensive" not in txt)
+# a="naruto".upper()
+# print(a)
+
+# # Strip() method is used to remove underscores placed before initial words
+# name="       Nischal Shrestha"
+# name1="Nischal Shrestha"
+# print(name.strip())
+# print(name1)
+
+
+# x=["apple","banana"]
+# y=["apple","lichhi"]
+x="apple"
+y="banana"
+z=x
+print(x is z )
+print(x is y)
---
+++ Python (DP+CFC)/tempCodeRunnerFile.py
@@ -0,0 +1,29 @@
+
+
+# for i in "Data":
+#     print(i)
+#     break
+# a="Data Science"
+# print(len(a))
+
+# # Boolean types
+# txt ="I ate Banana"
+# print("eat" in txt)
+
+
+# if "banana".capitalize() in txt:
+#     print("Banana is Present")
+# else:
+#     print("Banana is not Present")
+       
+# #Below is boolean type which is True Statements so It Returns True
+
+# print("expensive" not in txt)
+# a="naruto".upper()
+# print(a)
+
+# # Strip() method is used to remove underscores placed before initial words
+# name="       Nischal Shrestha"
+# name1="Nischal Shrestha"
+# print(name.strip())
+# print(name1)
